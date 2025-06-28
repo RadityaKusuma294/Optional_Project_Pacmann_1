c611cd317e433c988913463aa3807770244c1207
RadityaKusuma294 <kusumapradanaraditya@gmail.com>
2025-06-28 16:48:37 +0700

    sudah mulai lengkap

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from tabulate import tabulate
 class Transaction :
     def __init__(self):
         self.list_items = []
-        pass
 
     def add_item(self, nama_item, jumlah_barang, harga_per_item):
         barang = {"nama": nama_item, "jumlah barang": jumlah_barang, "harga": harga_per_item}
@@ -11,39 +10,53 @@ class Transaction :
         return self.list_items
     
     def update_item_name (self, nama_lama, nama_baru):
-        for i in range(len(self.list_items)):
-            if nama_lama in self.list_items[i]["nama"]:
-                self.list_items[i]["nama"] = nama_baru
-                print(self.list_items)
-                return f"{nama_lama} telah diganti dengan {nama_baru}."
-            continue
-        return "nama barang anda tidak tersedia."
-    
+        try:
+            for i in range(len(self.list_items)):
+                if nama_lama in self.list_items[i]["nama"]:
+                    self.list_items[i]["nama"] = nama_baru
+                    print(self.list_items)
+                    return f"{nama_lama} telah diganti dengan {nama_baru}."
+                else:
+                    raise ValueError ("nama barang anda tidak tersedia.")
+        except ValueError as e:
+            return e
+        
     def update_item_qty (self, nama_item, jumlah_baru):
-        for i in range(len(self.list_items)):
-            if nama_item in self.list_items[i]["nama"]:
-                self.list_items[i]["jumlah barang"] = jumlah_baru
-                print(self.list_items)
-                return f"jumlah produk {nama_item} telah diganti menjadi {jumlah_baru}."
-            continue
-        return "nama barang anda tidak tersedia."
+        try :
+            for i in range(len(self.list_items)):
+                if nama_item in self.list_items[i]["nama"]:
+                    self.list_items[i]["jumlah barang"] = jumlah_baru
+                    print(self.list_items)
+                    return f"jumlah produk {nama_item} telah diganti menjadi {jumlah_baru}."
+                
+            raise ValueError ("nama barang anda tidak tersedia.")
+        except ValueError as e:
+            return e
+        
     
     def update_item_price (self, nama_item, harga_baru):
-        for i in range(len(self.list_items)):
-            if nama_item in self.list_items[i]["nama"]:
-                self.list_items[i]["harga"] = harga_baru
-                print(self.list_items)
-                return f"harga produk {nama_item} telah diganti menjadi {harga_baru}."
-            continue
-        return "nama barang anda tidak tersedia."
+        try :    
+            for i in range(len(self.list_items)):
+                if nama_item in self.list_items[i]["nama"]:
+                    self.list_items[i]["harga"] = harga_baru
+                    print(self.list_items)
+                    return f"harga produk {nama_item} telah diganti menjadi {harga_baru}."
+                
+            raise ValueError ("nama barang anda tidak tersedia.")
+        except ValueError as e:
+            return e
     
     def delete_item (self, nama_item):
-        for i in range(len(self.list_items)):
-            if nama_item in self.list_items[i]["nama"]:
-                self.list_items.pop(i)
-                print(self.list_items)
-                return f"item {nama_item} telah terhapus"
-        return "nama barang anda tidak tersedia."
+        try: 
+            for i in range(len(self.list_items)):
+                if nama_item in self.list_items[i]["nama"]:
+                    self.list_items.pop(i)
+                    print(self.list_items)
+                    return f"item {nama_item} telah terhapus"
+                
+            raise ValueError ("nama barang anda tidak tersedia.")
+        except ValueError as e:
+            return e
     
     def reset_transaction (self):
         self.list_items.clear()
@@ -101,12 +114,82 @@ class Transaction :
 
 
 # input
-transct_123 = Transaction()
-transaksi_1 = transct_123.add_item("mobil", 1, 23_000_000)
-transaksi_2 = transct_123.add_item("motor", 3, 13_000_000)
-print(transaksi_2)
-print(transct_123.update_item_price("pakaian", 50_000_000))
-print("========================")
-print(transct_123.check_order())
-print(transct_123.total_price())
+    def main (self):
+        while True:
+            print()
+            print()
+            print("="*60)
+            print("MESIN KASIR SEDERHANA PACMANN")
+            print("="*60)
+            print("1. Input barang belanjaan")
+            print("2. Mengganti nama barang belanjaan")
+            print("3. Mengganti jumlah barang belanjaan")
+            print("4. Mengganti Harga barang belanjaan")
+            print("5. Tampilkan Daftar belanjaan")
+            print("6. Menghapus salah satu barang belanjaan" )
+            print("7. Reset belanjaan")
+            print("8. Total Belanjaan")
+            print("9. Exit\n")
+
+            choice = int(input('Masukkan Nomor Tugas : '))
+
+            try:
+
+                if choice == 1:
+                    nama_barang = input("Masukkan nama barang yang ingin anda beli: ")
+                    jumlah = int(input("Masukkan jumlah barang yang ingin anda beli: "))
+                    harga = int(input("Masukkan harga barang yang ingin anda beli: "))
+                    self.add_item(nama_barang, jumlah, harga)
+
+                elif choice == 2:
+                    nama_lama = input("Nama barang yang ingin di ganti: ")
+                    nama_baru = input("Nama barang baru: ")
+                    print(self.update_item_name(nama_lama, nama_baru))
+                
+                elif choice == 3:
+                    jumlah_lama = input("Jumlah barang yang ingin di ganti: ")
+                    jumlah_baru = input("Jumlah barang baru: ")
+                    print(self.update_item_name(jumlah_lama, jumlah_baru))
+                
+                elif choice == 4:
+                    harga_lama = input("Harga barang yang ingin di ganti: ")
+                    harga_baru = input("Harga barang baru: ")
+                    print(self.update_item_name(harga_lama, harga_baru))
+                
+                elif choice == 5:
+                    print(self.check_order())
+                
+                elif choice == 6:
+                    nama_dihapus = input("nama barang yang ingin dihapus: ")
+                    print(self.delete_item(nama_dihapus))
+                    print(self.check_order())
+                
+                elif choice == 7:
+                    print(self.reset_transaction())
+                    self.check_order()
+                
+                elif choice == 8:
+                    print(self.total_price())
+                
+                elif choice == 9:
+                    break
+
+                else:
+                    raise ValueError ("input anda salah")
+            
+            except ValueError as e:
+                return e
+            continue
+
+if __name__ == "__main__":
+    transaksi = Transaction()
+    print(transaksi.main())
+# transct_123 = Transaction()
+# transaksi_1 = transct_123.add_item("mobil", 1, 23_000_000)
+# transaksi_2 = transct_123.add_item("motor", 3, 13_000_000)
+# print(transaksi_2)
+# print(transct_123.update_item_price("pakaian", 50_000_000))
+# print("========================")
+# print(transct_123.check_order())
+# print(transct_123.total_price())
 
